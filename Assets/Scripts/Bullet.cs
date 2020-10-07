f9885023925646f0b8be085c201485e2899fbe4d
KyleNeubarth <kyle@neubarth.org>
2020-10-07 10:48:56 -0600

    add a gitignore

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Vector3 dir;
+    public float speed = 5;
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().MovePosition(transform.position+dir*Time.deltaTime*speed);
+        //transform.position += ;
+    }
+}
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    private Slider _slider;
+
+    public float fillSpeed = 1;
+
+    public int direction = 1;
+    
+    void Start()
+    {
+        _slider = GetComponent<Slider>();
+    }
+
+    void FixedUpdate()
+    { 
+        _slider.value += Time.deltaTime*fillSpeed*direction;
+    }
+
+    public bool IsFull()
+    {
+        return _slider.value == _slider.maxValue;
+    }
+    public bool IsEmpty()
+    {
+        return _slider.value == 0;
+    }
+}
